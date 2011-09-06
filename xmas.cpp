233d2d82b1b4d5ee465deb0cef5da708721ced0d
Christian von Kleist <cvonkleist@gmail.com>
2011-09-06 16:55:30 -0400

    initial commit

--- xmas.cpp
+++ xmas.cpp
@@ -1,137 +1,118 @@
 #include "WProgram.h"
 #include "xmas.h"
-// The delays in the begin, one, and zero functions look funny, but they give the correct
-// pulse durations when checked with a logic analyzer. Tested on an Arduino Uno.
-void xmas_begin(int strand)
-{
-	digitalWrite(strand_pin[strand], 1);
-	delayMicroseconds(7); //The pulse should be 10 uS long, but I had to hand tune the delays. They work for me
-	digitalWrite(strand_pin[strand], 0);
+
+// begin a serial packet on +strand+
+void xmas_begin(int strand) {
+  digitalWrite(strand_pin[strand], 1);
+  delayMicroseconds(7);
+  digitalWrite(strand_pin[strand], 0);
 }
 
-void xmas_one(int strand)
-{
-	digitalWrite(strand_pin[strand], 0);
-	delayMicroseconds(11); //This results in a 20 uS long low
-	digitalWrite(strand_pin[strand], 1);
-	delayMicroseconds(7);
-	digitalWrite(strand_pin[strand], 0);
+// send a serial 1 on +strand+
+void xmas_one(int strand) {
+  digitalWrite(strand_pin[strand], 0);
+  delayMicroseconds(11); // 20us low
+  digitalWrite(strand_pin[strand], 1);
+  delayMicroseconds(7);
+  digitalWrite(strand_pin[strand], 0);
 }
 
-void xmas_zero(int strand)
-{
-	digitalWrite(strand_pin[strand], 0);
-	delayMicroseconds(2);
-	digitalWrite(strand_pin[strand], 1);
-	delayMicroseconds(20 - 3);
-	digitalWrite(strand_pin[strand], 0);
+// send a serial 0 on +strand+
+void xmas_zero(int strand) {
+  digitalWrite(strand_pin[strand], 0);
+  delayMicroseconds(2);
+  digitalWrite(strand_pin[strand], 1);
+  delayMicroseconds(20 - 3);
+  digitalWrite(strand_pin[strand], 0);
 }
 
-void xmas_end(int strand)
-{
-	digitalWrite(strand_pin[strand],0);
-	delayMicroseconds(40); // Can be made shorter
-}
-
-
-// The rest of Robert's code is basically unchanged
-
-void xmas_fill_color(int strand, uint8_t begin,uint8_t count,uint8_t intensity,xmas_color_t color)
-{
-	while(count--)
-	{
-		xmas_set_color(strand, begin++,intensity,color);
-	}
-}
-
-void xmas_fill_color_same(int strand, uint8_t begin,uint8_t count,uint8_t intensity,xmas_color_t color)
-{
-	while(count--)
-	{
-		xmas_set_color(strand, 0,intensity,color);
-	}
+// end serial packet on +strand+
+void xmas_end(int strand) {
+  digitalWrite(strand_pin[strand],0);
+  delayMicroseconds(40);
 }
 
 
-void xmas_set_color(int strand, uint8_t led,uint8_t intensity,xmas_color_t color) {
-	uint8_t i;
-	xmas_begin(strand);
-	for(i=6;i;i--,(led<<=1))
-		if(led&(1<<5))
-			xmas_one(strand);
-		else
-			xmas_zero(strand);
-	for(i=8;i;i--,(intensity<<=1))
-		if(intensity&(1<<7))
-			xmas_one(strand);
-		else
-			xmas_zero(strand);
-	for(i=12;i;i--,(color<<=1))
-		if(color&(1<<11))
-			xmas_one(strand);
-		else
-			xmas_zero(strand);
-	xmas_end(strand);
+// set +count+ bulbs starting at +begin+ to +color+ at +intensity+
+void xmas_fill_color(int strand, uint8_t begin, uint8_t count, uint8_t intensity, xmas_color_t color) {
+  while(count--)
+    xmas_set_color(strand, begin++, intensity, color);
 }
 
-
-xmas_color_t
-xmas_color(uint8_t r,uint8_t g,uint8_t b) {
-	return COLOR(r,g,b);
+// set pixel number +led+ along a +strand+ to +color+ at +intensity+
+void xmas_set_color(int strand, uint8_t led, uint8_t intensity, xmas_color_t color) {
+  uint8_t i;
+  xmas_begin(strand);
+  for(i = 6; i; i--, (led <<= 1))
+    if(led & (1 << 5))
+      xmas_one(strand);
+    else
+      xmas_zero(strand);
+  for(i = 8; i; i--, (intensity <<= 1))
+    if(intensity & ( 1 << 7))
+      xmas_one(strand);
+    else
+      xmas_zero(strand);
+  for(i = 12; i; i--, (color <<= 1))
+    if(color & (1 << 11))
+      xmas_one(strand);
+    else
+      xmas_zero(strand);
+  xmas_end(strand);
 }
 
-xmas_color_t
-xmas_color_hue(uint8_t h) {
-	switch(h>>4) {
-		case 0:     h-=0; return xmas_color(h,CHANNEL_MAX,0);
-		case 1:     h-=16; return xmas_color(CHANNEL_MAX,(CHANNEL_MAX-h),0);
-		case 2:     h-=32; return xmas_color(CHANNEL_MAX,0,h);
-		case 3:     h-=48; return xmas_color((CHANNEL_MAX-h),0,CHANNEL_MAX);
-		case 4:     h-=64; return xmas_color(0,h,CHANNEL_MAX);
-		case 5:     h-=80; return xmas_color(0,CHANNEL_MAX,(CHANNEL_MAX-h));
-	}
+// returns a color value that represents an RGB triple
+xmas_color_t xmas_color(uint8_t r, uint8_t g, uint8_t b) {
+  return COLOR(r, g, b);
 }
 
+// clear the board
+//
+// when the pixels are freshly plugged in, this has the side effect of giving
+// each pixel an address, since a fresh pixel assumes the address of whatever
+// command it first hears (and then doesn't pass the command on, while it does
+// pass on all future commands it receives that are not addressed to it).
 void clear() {
-	for(int i = 0; i < STRAND_COUNT; i++) {
-		pinMode(strand_pin[i], OUTPUT);
-		xmas_fill_color(i, 0, LIGHT_COUNT, DEFAULT_INTENSITY, COLOR_BLACK); //Enumerate all the lights
-	}
+  for(int i = 0; i < STRAND_COUNT; i++) {
+    pinMode(strand_pin[i], OUTPUT);
+    xmas_fill_color(i, 0, LIGHT_COUNT, DEFAULT_INTENSITY, COLOR_BLACK);
+  }
 }
-
-#define WIDTH 10
-#define HEIGHT 10
 
 inline uint8_t translate(int x, int y);
 inline uint8_t translate_strand(int x, int y);
-extern inline void set_pixel(uint8_t x,uint8_t y, uint8_t intensity, xmas_color_t color);
+extern inline void set_pixel(uint8_t x, uint8_t y, uint8_t intensity, xmas_color_t color);
 
+// swap the variables pointed at by *a and *b
 static inline void swap(uint8_t *a, uint8_t *b) {
-	uint8_t t;
-	t = *a;
-	*b = *a;
-	*a = t;
+  uint8_t t;
+  t = *a;
+  *b = *a;
+  *a = t;
 }
 
+// draw a line from (x0, y0) to (x1, y1)
+//
+// efficient line implementation found in wikipedia
 inline void line(uint8_t x0, uint8_t y0, uint8_t x1, uint8_t y1, uint8_t intensity, xmas_color_t c) {
-	int dx = abs(x1-x0);
-	int dy = abs(y1-y0);
-	int sy, sx;
-	if(x0 < x1)sx = 1;else sx = -1;
-	if(y0 < y1)sy = 1;else sy = -1;
-	int err = dx-dy;
+  int dx = abs(x1 - x0);
+  int dy = abs(y1 - y0);
+  int sy, sx;
+  if(x0 < x1) sx = 1; else sx = -1;
+  if(y0 < y1) sy = 1; else sy = -1;
+  int err = dx - dy;
 
-	while(1) {
-		set_pixel(x0,y0, intensity, c);
-		if(x0 == x1 && y0 == y1) break;
-		int e2 = 2*err;
-		if(e2 > -dy) {
-			err = err - dy;
-			x0 = x0 + sx;
-		}
-		if(e2 <  dx) {
-			err = err + dx;
-			y0 = y0 + sy;
-		}
-	}
+  while(1) {
+    set_pixel(x0, y0, intensity, c);
+    if(x0 == x1 && y0 == y1) break;
+    int e2 = 2 * err;
+    if(e2 > -dy) {
+      err = err - dy;
+      x0 = x0 + sx;
+    }
+    if(e2 < dx) {
+      err = err + dx;
+      y0 = y0 + sy;
+    }
+  }
 }
--- xmas.h
+++ xmas.h
@@ -1,77 +1,83 @@
-// GE Christmas light control for Arduino
-// Ported by Scott Harris <scottrharris@gmail.com>
-// scottrharris.blogspot.com
+// ge g-35 xmas library
+// cvk/2011
+//
+// based on scott harris' code, which was based on deepdarc's code. these
+// notices appeared in this file:
+//
+//  // GE Christmas light control for Arduino
+//  // Ported by Scott Harris <scottrharris@gmail.com>
+//  // scottrharris.blogspot.com
+//  //
+//  /*!     Christmas Light Control
+//   **     By Robert Quattlebaum <darco@deepdarc.com>
+//   **     Released November 27th, 2010
+//   **
+//   **     For more information,
+//   **     see <http://www.deepdarc.com/2010/11/27/hacking-christmas-lights/>.
+//   **
+//   **     Originally intended for the ATTiny13, but should
+//   **     be easily portable to other microcontrollers.
+//   */
 
 #include "WProgram.h"
 
 
-// Based on this code:
 
-/*!     Christmas Light Control
- **     By Robert Quattlebaum <darco@deepdarc.com>
- **     Released November 27th, 2010
- **
- **     For more information,
- **     see <http://www.deepdarc.com/2010/11/27/hacking-christmas-lights/>.
- **
- **     Originally intended for the ATTiny13, but should
- **     be easily portable to other microcontrollers.
- */
+#define xmas_color_t uint16_t
 
-#define xmas_color_t uint16_t // typedefs can cause trouble in the Arduino environment
-
-// Eliminate the .h file
-
-#define LIGHT_COUNT          (50) //I only have a 36 light strand. Should be 50 or 36
-#define CHANNEL_MAX          (0xF)
-#define DEFAULT_INTENSITY     (0xCC)
-#define HUE_MAX               ((CHANNEL_MAX+1)*6-1)
-#define COLOR(r,g,b)     ((r)+((g)<<4)+((b)<<8))
-#define COLOR_WHITE     COLOR(CHANNEL_MAX,CHANNEL_MAX,CHANNEL_MAX)
-#define COLOR_BLACK     COLOR(0,0,0)
-#define COLOR_RED          COLOR(CHANNEL_MAX,0,0)
-#define COLOR_GREEN     COLOR(0,CHANNEL_MAX,0)
-#define COLOR_BLUE          COLOR(0,0,CHANNEL_MAX)
-#define COLOR_CYAN          COLOR(0,CHANNEL_MAX,CHANNEL_MAX)
-#define COLOR_MAGENTA     COLOR(CHANNEL_MAX,0,CHANNEL_MAX)
-#define COLOR_YELLOW     COLOR(CHANNEL_MAX,CHANNEL_MAX,0)
+#define LIGHT_COUNT (50)
+#define CHANNEL_MAX (0xF)
+#define DEFAULT_INTENSITY (0xCC)
+#define HUE_MAX ((CHANNEL_MAX+1)*6-1)
+#define COLOR(r,g,b) ((r)+((g)<<4)+((b)<<8))
+#define COLOR_WHITE COLOR(CHANNEL_MAX,CHANNEL_MAX,CHANNEL_MAX)
+#define COLOR_BLACK COLOR(0,0,0)
+#define COLOR_RED COLOR(CHANNEL_MAX,0,0)
+#define COLOR_GREEN COLOR(0,CHANNEL_MAX,0)
+#define COLOR_BLUE COLOR(0,0,CHANNEL_MAX)
+#define COLOR_CYAN COLOR(0,CHANNEL_MAX,CHANNEL_MAX)
+#define COLOR_MAGENTA COLOR(CHANNEL_MAX,0,CHANNEL_MAX)
+#define COLOR_YELLOW COLOR(CHANNEL_MAX,CHANNEL_MAX,0)
 
 // Pin setup
 #define STRAND_COUNT 2
 #define STRAND_PINS {4, 6}
 #define STATUSPIN 13 // The LED
 
-const int strand_pin[STRAND_COUNT] = STRAND_PINS;
+#define WIDTH 10
+#define HEIGHT 10
 
-// The delays in the begin, one, and zero functions look funny, but they give the correct
-// pulse durations when checked with a logic analyzer. Tested on an Arduino Uno.
+const int strand_pin[STRAND_COUNT] = STRAND_PINS;
 
 void xmas_begin(int strand);
 void xmas_one(int strand);
 void xmas_zero(int strand);
 void xmas_end(int strand);
-void xmas_fill_color(int strand, uint8_t begin,uint8_t count,uint8_t intensity,xmas_color_t color);
-void xmas_fill_color_same(int strand, uint8_t begin,uint8_t count,uint8_t intensity,xmas_color_t color);
-void xmas_set_color(int strand, uint8_t led,uint8_t intensity,xmas_color_t color);
-xmas_color_t xmas_color(uint8_t r,uint8_t g,uint8_t b);
-xmas_color_t xmas_color_hue(uint8_t h);
+void xmas_fill_color(int strand, uint8_t begin, uint8_t count, uint8_t intensity, xmas_color_t color);
+void xmas_fill_color_same(int strand, uint8_t begin, uint8_t count, uint8_t intensity, xmas_color_t color);
+void xmas_set_color(int strand, uint8_t led, uint8_t intensity, xmas_color_t color);
+xmas_color_t xmas_color(uint8_t r, uint8_t g, uint8_t b);
 void clear();
 
-#define WIDTH 10
-#define HEIGHT 10
+// figured out my problem with inlining with
+// http://www.velocityreviews.com/forums/t437213-extern-inline.html
 
-// figured out how to inline stuff from http://www.velocityreviews.com/forums/t437213-extern-inline.html
+// returns the strand number for coordinate (x, y)
 inline uint8_t translate_strand(int x, int y) {
-	return x % STRAND_COUNT;
+  return x % STRAND_COUNT;
 }
+
+// returns the bulb number that corresponds to coordinate (x, y)
 inline uint8_t translate(int x, int y) {
-	int n;
-	n = (x - translate_strand(x, y)) / STRAND_COUNT * HEIGHT;
-	n += (n / HEIGHT) % 2 == 1 ?  y : (HEIGHT - y - 1);
-	return n;
+  int n;
+  n = (x - translate_strand(x, y)) / STRAND_COUNT * HEIGHT;
+  n += (n / HEIGHT) % 2 == 1 ?  y : (HEIGHT - y - 1);
+  return n;
 }
-inline void set_pixel(uint8_t x,uint8_t y, uint8_t intensity, xmas_color_t color) {
-	xmas_set_color(translate_strand(x, y), translate(x, y), intensity, color);
+
+// turn the pixel (x, y) on to +intensity+ with +color+
+inline void set_pixel(uint8_t x, uint8_t y, uint8_t intensity, xmas_color_t color) {
+  xmas_set_color(translate_strand(x, y), translate(x, y), intensity, color);
 }
 static inline void swap(uint8_t *a, uint8_t *b);
 inline void line(uint8_t x0, uint8_t y0, uint8_t x1, uint8_t y1, uint8_t intensity, xmas_color_t c);
